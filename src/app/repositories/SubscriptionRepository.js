04ea2beedd104f4205006fe5b3c8b77b96a89bc4
henriquemilitao <henriquemilitao35@gmail.com>
2024-06-16 14:33:48 -0400

    Adiciona repositório e serviço para gerenciar inscrições de newsletter

---
+++ src/app/repositories/SubscriptionRepository.js
@@ -0,0 +1,21 @@
+import Subscriptio from "../models/Subscription";
+
+class SubscriptionRepository {
+    async create(subscriptionData) {
+        return await Subscription.create(subscriptionData)
+    }
+
+    async findAll() {
+        return await Subscription.findAll()
+    }
+
+    async findByEmail(email) {
+        return await Subscription.findOne({ where: { email } })
+    }
+
+    async deleteByEmail(email) {
+        return await Subscription.destroy({ where: { email } })
+    }
+}
+
+export default new SubscriptionRepository()
---
+++ src/app/services/SubscriptionService.js
@@ -0,0 +1,28 @@
+import SubscriptionRepository from "../repositories/SubscriptionRepository"
+
+class SubscriptionService {
+    async register(email, frequency) {
+        const existingSubscription = await SubscriptionRepository.findByEmail(email)
+        if (existingSubscription) {
+            throw new Error('Email already subscribed')
+        }
+
+        return await SubscriptionRepository.create({ email, frequency })
+    }
+
+    async unregister(email) {
+        const existingSubscription = await SubscriptionRepository.findByEmail(email)
+        if (!existingSubscription) {
+            throw new Error('Email not found')
+        }
+
+        await SubscriptionRepository.deleteByEmail(email)
+        return { message: 'Unsubscribed successfully' }
+    }
+
+    async listSubscriptions() {
+        return await SubscriptionRepository.findAll()
+    }
+}
+
+export default new SubscriptionService();
