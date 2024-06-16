44def2d421e0a7feb32e45d91a1c63ea052eab18
henriquemilitao <henriquemilitao35@gmail.com>
2024-06-16 12:10:26 -0400

    Adiciona migration para tabela de inscrições de newsletter com enum de frequência

---
+++ src/database/migrations/20240616160045-create-subscriptions.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('subscriptions', { 
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      frequency: {
+        type: Sequelize.ENUM,
+        values: ['weekly', 'biweekly', 'monthly', 'semiannually'],
+        allowNull: false,
+      },
+      createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+      },
+      updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+      },
+    });
+     
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('subscriptions');
+  }
+};
